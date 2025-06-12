ac7cd2857381f746b60399408a13e96b3f359dda
Grod56 <rodneygaryx29@gmail.com>
2025-06-12 23:22:47 +0200

    bug fixes

--- src/lib/components/site-subsection/site-subsection-model.ts
+++ src/lib/components/site-subsection/site-subsection-model.ts
@@ -1,4 +1,3 @@
-import { ContentComponentModelView } from "@/app-library/components/content/content-model";
 import { ReadonlyModel } from "@mvc-react/mvc";
 
 export type SiteSubsectionModel = ReadonlyModel<SiteSubsectionModelView>;
