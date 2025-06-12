0da15d2dee81c375ca46d37b58a49daf920be7b0
Grod56 <rodneygaryx29@gmail.com>
2025-06-12 23:17:41 +0200

    bug fixes

--- src/app/api/readings/route.ts
+++ src/app/api/readings/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest } from "next/server";
-import { getSensorReadingsData } from "./utility";
+import { getReadings } from "./utility";
 
 export async function GET(req: NextRequest) {
 	const requestURL = new URL(req.url);
 	const type = requestURL.searchParams.get("type");
-	return new Response(JSON.stringify(await getSensorReadingsData(type)), {
+	return new Response(JSON.stringify(await getReadings(type)), {
 		status: 201,
 		headers: { "Content-Type": "application/json" },
 	});
