ac9466eedc33c46dbd09f93d5a6a2b407159a7d0
Grod56 <rodneygaryx29@gmail.com>
2025-07-02 09:19:57 +0200

    misc

--- src/lib/components/header/Header.tsx
+++ src/lib/components/header/Header.tsx
@@ -4,8 +4,11 @@ const Header = () => {
 	return (
 		<div className="header">
 			<div className="header-content">
-				<img className="header-icon" src="/watering-plants.png" />
-				<span className="header-title">Intelligent Irrigation</span>
+				<div className="logo">
+					<img className="header-icon" src="/watering-plants.png" />
+					<span className="header-title">Intelligent Irrigation</span>
+				</div>
+				<a href="https://youtu.be/i36kXP_ZwiE">View Demo</a>
 			</div>
 		</div>
 	);
